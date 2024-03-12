0b52d902417fbb5777c67a41826b0afc633fd665
えんでばー <pro.onaka.hetta@gmail.com>
2024-03-12 16:20:37 +0900

    Remove navigator key and related files

--- lib/router/core/navigator_key.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'navigator_key.g.dart';
-
-@riverpod
-GlobalKey<NavigatorState> navigatorKey(NavigatorKeyRef ref) {
-  return GlobalKey<NavigatorState>();
-}
--- lib/router/core/router.dart
+++ lib/router/core/router.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:go_router_practice/router/core/navigator_key.dart';
 import 'package:go_router_practice/router/routes/home/chat/chat_route.dart';
 import 'package:go_router_practice/router/routes/top_routes.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'router.g.dart';
 
+final rootNavigatorKey = GlobalKey<NavigatorState>();
+
 @riverpod
 GoRouter router(RouterRef ref) {
-  final navigatorKey = ref.watch(navigatorKeyProvider);
   return GoRouter(
-    navigatorKey: navigatorKey,
+    navigatorKey: rootNavigatorKey,
     debugLogDiagnostics: kDebugMode,
     routes: $appRoutes,
     initialLocation: ChatRoute().location,
